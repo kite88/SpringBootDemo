4f1951a1761bfe49293e63819d347a3c8319db3a
kite0101 <hjs720@qq.com>
2020-10-16 09:53:12 +0800

    模块api

---
+++ src/main/java/org/example/server/user/api/impl/HomeImplApiUser.java
@@ -0,0 +1,9 @@
+package org.example.server.user.api.impl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class HomeImplApiUser {
+}
---
+++ src/main/java/org/example/server/user/api/valid/HomeValidApiUser.java
@@ -0,0 +1,4 @@
+package org.example.server.user.api.valid;
+
+public class HomeValidApiUser {
+}
